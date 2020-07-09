173b60070dc6b33d2ed2c7dfb42c60183821e6cd
Steve <I1413735@continental.edu.pe>
2020-07-09 16:13:04 -0500

    Interface Registro

--- app/src/main/java/com/example/masterdent/Registro.java
+++ app/src/main/java/com/example/masterdent/Registro.java
@@ -1,12 +1,14 @@
 package com.example.masterdent;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class IniciarSesion extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_iniciar_sesion);
+        setContentView(R.layout.activity_registro);
     }
 }

ba2e810c7121521d6336f271cbf3c19630fd3458
Steve <I1413735@continental.edu.pe>
2020-07-09 17:57:14 -0500

    Cambios En Registro de Usuario

--- app/src/main/java/com/example/masterdent/IniciarSesion.java
+++ app/src/main/java/com/example/masterdent/IniciarSesion.java
@@ -2,7 +2,10 @@ package com.example.masterdent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IniciarSesion extends AppCompatActivity {
 
@@ -11,7 +14,14 @@ public class IniciarSesion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
 
+        Button irARegistro = findViewById(R.id.btnRegistarse);
+        irARegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent o = new Intent(IniciarSesion.this, RegistroUsuario.class);
+                startActivity(o);
+            }
+        });
         }
-
     }
 
